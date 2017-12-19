982f893a9f52a508cb21d7434ce5325d4c3ac407
MichaelPay <Michael.Pay@mail.com>
2017-12-19 04:03:18 -0800

    01 - Flask Basics 01 - Hello World with Query

--- 01 - Flask Basics - TeamTreeHouse Course/01 - Welcome to Flask/simple_app.py
+++ 01 - Flask Basics - TeamTreeHouse Course/01 - Welcome to Flask/simple_app.py
@@ -1,4 +1,11 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
+@app.route('/')
+def index(name="Michael"):
+    name = request.args.get('name', name)
+    return f"Hello, {name}"
+
+app.run(debug=True, port = 8080, host= '0.0.0.0')
