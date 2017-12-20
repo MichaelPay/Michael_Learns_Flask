59116b5b0aa4d00c4a13b0ddd55b9423e967678a
MichaelPay <monickenish@gmail.com>
2017-12-19 19:19:06 -0800

    01 Flask Basics 01 - Templates - Hand-coded HTML method

--- 01 - Flask Basics - TeamTreeHouse Course/01 - Welcome to Flask/simple_app.py
+++ 01 - Flask Basics - TeamTreeHouse Course/01 - Welcome to Flask/simple_app.py
@@ -1,29 +1,50 @@
 # from flask import Flask
-#
 # app = Flask(__name__)
 #
-# @app.route('/')
-# @app.route('/<name>')
-# def index(name="Michael"):
-#     return f"Hello, {name}"
 #
-#
-# @app.route("/add/<int:num1>/<int:num2>/")
-# def add(num1, num2):
-#     answer = num1 + num2
-#     return f"{str(num1)} + {str(num2)} = {str(answer)}"
-#
-# app.run(debug=True, port = 8080, host= '0.0.0.0')
+# @app.route("/multiply")
+# @app.route("/multiply/<num1>/<num2>/")
+# def multiply(num1 = 5, num2=5):
+#     answer = num1 * num2
+#     return str(answer)
 
 from flask import Flask
+
 app = Flask(__name__)
 
+@app.route('/')
+@app.route('/<name>')
+def index(name="Michael"):
+    return f"Hello, {name}"
 
-@app.route("/multiply")
-@app.route("/multiply/<num1>/<num2>/")
-def multiply(num1 = 5, num2=5):
-    answer = num1 * num2
-    return str(answer)
+
+@app.route("/add/<int:num1>/<int:num2>/")
+def add(num1, num2):
+    answer = num1 + num2
+    return f"""
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Adding!
+            </title>
+        </head>
+        
+        <body>
+        <h1>
+        {num1} + {num2} = { num1 + num2 }
+        </h1>
+        </body>
+    
+    </html>
+    
+    
+    """
+
+
+
+app.run(debug=True, port = 8080, host= '0.0.0.0')
+
+
 
 
 
