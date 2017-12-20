9c060f80d33f4f4e7ba8dcddd0413f0051a074b4
MichaelPay <Michael.Pay@mail.com>
2017-12-19 18:39:17 -0800

    01 - Flask Basics 01 - Challenge 1

--- 01 - Flask Basics - TeamTreeHouse Course/01 - Welcome to Flask/simple_app.py
+++ 01 - Flask Basics - TeamTreeHouse Course/01 - Welcome to Flask/simple_app.py
@@ -1,11 +1,29 @@
-from flask import Flask
-from flask import request
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+# @app.route('/')
+# @app.route('/<name>')
+# def index(name="Michael"):
+#     return f"Hello, {name}"
+#
+#
+# @app.route("/add/<int:num1>/<int:num2>/")
+# def add(num1, num2):
+#     answer = num1 + num2
+#     return f"{str(num1)} + {str(num2)} = {str(answer)}"
+#
+# app.run(debug=True, port = 8080, host= '0.0.0.0')
 
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def index(name="Michael"):
-    name = request.args.get('name', name)
-    return f"Hello, {name}"
 
-app.run(debug=True, port = 8080, host= '0.0.0.0')
+@app.route("/multiply")
+@app.route("/multiply/<num1>/<num2>/")
+def multiply(num1 = 5, num2=5):
+    answer = num1 * num2
+    return str(answer)
+
+
+
